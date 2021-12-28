e39ec21b71945a823c564cfc75b508fb135acdee
Satont <satontworldwide@gmail.com>
2021-12-28 18:18:04 +0300

    chore: add eggs release tool

---
+++ eggs.ts
@@ -0,0 +1,29 @@
+import { publish } from 'https://x.nest.land/eggs@0.3.10/src/commands/publish.ts';
+
+const pkgDir = `./packages/${Deno.args[0]}`;
+Deno.chdir(pkgDir);
+
+const pkg = JSON.parse(Deno.readTextFileSync(`./package.json`));
+
+const config = {
+  'description': pkg.description,
+  'homepage': pkg.homepage,
+  'version': pkg.version,
+  'entry': `./src/mod.ts`,
+  'unstable': false,
+  'unlisted': false,
+  'checkFormat': false,
+  'checkTests': false,
+  'checkInstallation': false,
+  'check': true,
+  'files': [
+    `./src/*.ts`,
+    `LICENSE`,
+    `README.md`,
+  ],
+  'ignore': [
+    `./src/deps.node.ts`,
+  ],
+};
+
+publish(config, pkg.name.replace('@satont/', ''));
