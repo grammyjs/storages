e5dbe5ca0180f2e35119675dc9865df1546660c8
Satont <satontworldwide@gmail.com>
2021-12-28 20:03:50 +0300

    fix(release): add missed --allow-net flag to eggs

--- eggs.ts
+++ eggs.ts
@@ -1,9 +1,13 @@
 import { publish } from 'https://x.nest.land/eggs@0.3.10/src/commands/publish.ts';
 
+// import example config for autocomplitions
+const ExamplePkg = await import('./packages/redis/package.json', { assert: { type: 'json' } });
+type Pkg = typeof ExamplePkg
+
 const pkgDir = `./packages/${Deno.args[0]}`;
 Deno.chdir(pkgDir);
 
-const pkg = JSON.parse(Deno.readTextFileSync(`./package.json`));
+const pkg = JSON.parse(Deno.readTextFileSync(`./package.json`)) as Pkg;
 
 publish({
   'description': pkg.description,
@@ -22,7 +26,7 @@ publish({
     `README.md`,
   ],
   'ignore': [
-    `./src/deps.node.ts`,
+    `./src/*.node.ts`,
   ],
   yes: true,
 }, pkg.name.replace('@satont/', ''));
