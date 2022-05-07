8459565d6677b902474f92e6aac7247e39cd43ea
Satont <satontworldwide@gmail.com>
2022-05-07 15:15:45 +0300

    chore: refactor tests to use node:tests runner

--- packages/file/vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  test: {
-    include: ['__tests__/node.ts'],
-    testTimeout: 2000,
-    watch: false,
-  },
-});
--- packages/psql/__tests__/node.ts
+++ packages/psql/__tests__/node.ts
@@ -1,68 +1,52 @@
-import { describe, test, expect } from 'vitest';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import test from 'node:test';
+import assert from 'node:assert';
+
 import { newDb } from 'pg-mem';
 import { PsqlAdapter } from '../dist/cjs/mod';
 import { session } from 'grammy';
 import * as utils from '@grammyjs/storage-utils';
 
-describe('Pizza counter test', () => {
-  test('Pizza counter should be equals 0 on initial', async () => {
-    const bot = utils.createBot();
-    const ctx = utils.createMessage(bot);
-    const client = new (newDb().adapters.createPg().Client);
+test('Pizza counter test', async () => {
+  const bot = utils.createBot();
+  const client = new (newDb().adapters.createPg().Client);
 
-    bot.use(session({
-      initial:() => ({ pizzaCount: 0 }),
-      storage: await PsqlAdapter.create({ tableName: 'sessions', client }),
-    }));
+  bot.use(session({
+    initial: () => ({ pizzaCount: 0 }),
+    storage: await PsqlAdapter.create({ tableName: 'sessions', client }),
+  }));
 
-    await bot.handleUpdate(ctx.update);
-
-    bot.on('msg:text', (ctx) => {
-      expect(ctx.session.pizzaCount).toEqual(0);
-    });
+  bot.hears('first', (ctx) => {
+    assert.equal(ctx.session.pizzaCount, 0);
+    ctx.session.pizzaCount = Number(ctx.session.pizzaCount) + 1;
   });
-
-  test('Pizza counter should be equals 1 after first message', async () => {
-    const bot = utils.createBot();
-    const client = new (newDb().adapters.createPg().Client);
-
-    bot.use(session({
-      initial: () => ({ pizzaCount: 0 }),
-      storage: await PsqlAdapter.create({ tableName: 'sessions', client }),
-    }));
-
-    bot.hears('first', (ctx) => {
-      ctx.session.pizzaCount = Number(ctx.session.pizzaCount) + 1;
-    });
-    
-    bot.hears('second', (ctx) => {
-      expect(ctx.session.pizzaCount).toEqual(1);
-    });
-    
-    await bot.handleUpdate(utils.createMessage(bot, 'first').update);
-    await bot.handleUpdate(utils.createMessage(bot, 'second').update);
+  
+  bot.hears('second', (ctx) => {
+    assert.equal(ctx.session.pizzaCount, 1);
   });
+  
+  await bot.handleUpdate(utils.createMessage(bot, 'first').update);
+  await bot.handleUpdate(utils.createMessage(bot, 'second').update);
 });
 
-describe('Simple string test', () => {
-  test('Should be changed', async () => {
-    const client = new (newDb().adapters.createPg().Client);
-    const bot = utils.createBot(false);
+test('Should be changed', async () => {
+  const client = new (newDb().adapters.createPg().Client);
+  const bot = utils.createBot(false);
 
-    bot.use(session({
-      initial: () => 'test',
-      storage: await PsqlAdapter.create({ tableName: 'sessions', client }),
-    }));
+  bot.use(session({
+    initial: () => 'test',
+    storage: await PsqlAdapter.create({ tableName: 'sessions', client }),
+  }));
 
-    bot.hears('first', async (ctx) => {
-      ctx.session = `${ctx.session} edited`;
-    });
-    
-    bot.hears('second', async (ctx) => {
-      expect(ctx.session).toEqual('test edited');
-    });
-    
-    await bot.handleUpdate(utils.createMessage(bot, 'first').update);
-    await bot.handleUpdate(utils.createMessage(bot, 'second').update);
+  bot.hears('first', async (ctx) => {
+    ctx.session = `${ctx.session} edited`;
   });
+  
+  bot.hears('second', async (ctx) => {
+    assert.equal(ctx.session, 'test edited');
+  });
+  
+  await bot.handleUpdate(utils.createMessage(bot, 'first').update);
+  await bot.handleUpdate(utils.createMessage(bot, 'second').update);
 });
