65f936f932ca44907499daa545adb7198c1915fa
GitHub <noreply@github.com>
2023-01-06 01:13:52 +0000

    chore: update deno dependencies

--- packages/redis/src/deps.deno.ts
+++ packages/redis/src/deps.deno.ts
@@ -1,2 +1,2 @@
-export type { Redis as Client, RedisConnectOptions } from 'https://deno.land/x/redis@v0.28.0/mod.ts';
+export type { Redis as Client, RedisConnectOptions } from 'https://deno.land/x/redis@v0.29.0/mod.ts';
 export type { StorageAdapter } from 'https://deno.land/x/grammy@v1.13.0/mod.ts';
