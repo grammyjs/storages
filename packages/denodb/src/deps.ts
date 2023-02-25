6b8742e14a76d27e1bf4900af10dd1fd83a75d5e
Satont <satontworldwide@gmail.com>
2023-02-25 11:31:54 +0300

    chore(denodb): change version to 1.x because anyway latest commit not working

--- packages/denodb/src/deps.ts
+++ packages/denodb/src/deps.ts
@@ -1,2 +1,2 @@
 export type { StorageAdapter } from 'https://lib.deno.dev/x/grammy@1.x/mod.ts';
-export * as DenoDB from 'https://raw.githubusercontent.com/eveningkid/denodb/09db670070e9e33e26729a36aeab6b9f3c372ee6/mod.ts';
+export * as DenoDB from 'https://lib.deno.dev/x/denodb@1.x/mod.ts';
