b59428bbac72521c31941969bef44062a50b4fa3
Satont <satontworldwide@gmail.com>
2023-10-09 22:22:01 +0300

    feat(cloudflare): add `d1` adapter (#202)
    * feat(cloudflare): add `d1` adapter
    
    * Initial commit (by create-cloudflare CLI)
    
    * feat(cloudflare): add `d1` adapter
    
    * chore(cloudflare): remove token from d1 example

---
+++ packages/cloudflare/examples/d1/src/index.ts
@@ -0,0 +1,47 @@
+import { D1Database } from '@cloudflare/workers-types'
+import {
+	type Context,
+	type SessionFlavor,
+	Bot,
+	session,
+	webhookCallback
+} from 'grammy/web'
+
+import { D1Adapter } from '../../../src/index.js'
+
+export interface Env {
+	BOT_TOKEN: string
+	DB: D1Database;
+}
+
+interface SessionData {
+	timestamp: string;
+}
+type MyContext = Context & SessionFlavor<SessionData>;
+
+export default {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		const grammyD1StorageAdapter = await D1Adapter.create<SessionData>(env.DB, 'GrammySessions')
+
+		const bot = new Bot<MyContext>(env.BOT_TOKEN);
+
+		bot.use(session({
+			initial: (): SessionData => {
+				return { timestamp: "World" }
+			},
+			storage: grammyD1StorageAdapter,
+		}))
+
+		bot.use(async (ctx, next) => {
+			ctx.session.timestamp = Date.now().toString();
+
+			await next();
+		})
+
+		bot.command("start", async (ctx: MyContext) => {
+			await ctx.reply(`Hello at ${ctx.session.timestamp}`);
+		})
+
+		return webhookCallback(bot, "cloudflare-mod")(request);
+	},
+};
