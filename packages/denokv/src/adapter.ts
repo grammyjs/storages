25d35a237e5213737cd5b771b5736019547e98d7
Ubertao <ubertao@users.noreply.github.com>
2024-05-28 08:30:29 +0800

    add prefix support (#211)
    allow custom prefix to store session data under specified key space

--- packages/denokv/src/adapter.ts
+++ packages/denokv/src/adapter.ts
@@ -1,18 +1,19 @@
 import { StorageAdapter } from './deps.ts';
 
 export class DenoKVAdapter<T> implements StorageAdapter<T> {
-  constructor(private kv: Deno.Kv) {}
+  constructor(private kv: Deno.Kv, 
+              private prefix: Deno.KvKeyPart[] = ['sessions']) {}
 
   async read(key: string): Promise<T | undefined> {
-    const result = await this.kv.get(['sessions', key]);
+    const result = await this.kv.get([...this.prefix, key]);
     return result.value !== null ? result.value as T : undefined;
   }
 
   async write(key: string, value: T) {
-    await this.kv.set(['sessions', key], value);
+    await this.kv.set([...this.prefix, key], value);
   }
 
   async delete(key: string) {
-    await this.kv.delete(['sessions', key]);
+    await this.kv.delete([...this.prefix, key]);
   }
 }
