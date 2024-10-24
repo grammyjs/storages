4a11d64c81ff52613cbf55c57382b4970b27b478
Satont <satontworldwide@gmail.com>
2024-10-24 11:47:21 +0300

    update denokv storage

--- packages/denokv/test/session.test.ts
+++ packages/denokv/test/session.test.ts
@@ -1,55 +1,55 @@
 import { DenoKVAdapter } from '../src/adapter.ts';
-import { session } from 'https://lib.deno.dev/x/grammy@1.x/mod.ts';
+import { session } from 'grammy';
 import { expect } from 'https://deno.land/x/expect@v0.4.0/mod.ts';
-import { createBot, createMessage } from '../../../libs/utils/src/mod.ts';
+import { createBot, createMessage } from '@grammyjs/storage-utils';
 
 Deno.test('Simple string tests', async () => {
-  const tempFilePath = await Deno.makeTempFile();
-  const bot = createBot(false);
-  const kv = await Deno.openKv(tempFilePath);
+	const tempFilePath = await Deno.makeTempFile();
+	const bot = createBot(false);
+	const kv = await Deno.openKv(tempFilePath);
 
-  bot.use(session({
-    initial: () => 'test',
-    storage: new DenoKVAdapter(kv),
-  }));
+	bot.use(session({
+		initial: () => 'test',
+		storage: new DenoKVAdapter(kv),
+	}));
 
-  bot.hears('first', (ctx) => {
-    ctx.session = `${ctx.session} edited`;
-  });
+	bot.hears('first', (ctx) => {
+		ctx.session = `${ctx.session} edited`;
+	});
 
-  bot.hears('second', (ctx) => {
-    expect(ctx.session).toEqual('test edited');
-  });
+	bot.hears('second', (ctx) => {
+		expect(ctx.session).toEqual('test edited');
+	});
 
-  await bot.handleUpdate(createMessage(bot, 'first').update);
-  await bot.handleUpdate(createMessage(bot, 'second').update);
+	await bot.handleUpdate(createMessage(bot, 'first').update);
+	await bot.handleUpdate(createMessage(bot, 'second').update);
 
-  await kv.close();
-  Deno.removeSync(tempFilePath);
+	await kv.close();
+	Deno.removeSync(tempFilePath);
 });
 
 Deno.test('Pizza counter tests', async () => {
-  const tempFilePath = await Deno.makeTempFile();
-  const bot = createBot();
-  const kv = await Deno.openKv(tempFilePath);
+	const tempFilePath = await Deno.makeTempFile();
+	const bot = createBot();
+	const kv = await Deno.openKv(tempFilePath);
 
-  bot.use(session({
-    initial: () => ({ pizzaCount: 0 }),
-    storage: new DenoKVAdapter(kv),
-  }));
+	bot.use(session({
+		initial: () => ({ pizzaCount: 0 }),
+		storage: new DenoKVAdapter(kv),
+	}));
 
-  bot.hears('first', (ctx) => {
-    expect(ctx.session.pizzaCount).toEqual(0);
-    ctx.session.pizzaCount = Number(ctx.session.pizzaCount) + 1;
-  });
+	bot.hears('first', (ctx) => {
+		expect(ctx.session.pizzaCount).toEqual(0);
+		ctx.session.pizzaCount = Number(ctx.session.pizzaCount) + 1;
+	});
 
-  bot.hears('second', (ctx) => {
-    expect(ctx.session.pizzaCount).toEqual(1);
-  });
+	bot.hears('second', (ctx) => {
+		expect(ctx.session.pizzaCount).toEqual(1);
+	});
 
-  await bot.handleUpdate(createMessage(bot, 'first').update);
-  await bot.handleUpdate(createMessage(bot, 'second').update);
+	await bot.handleUpdate(createMessage(bot, 'first').update);
+	await bot.handleUpdate(createMessage(bot, 'second').update);
 
-  await kv.close();
-  Deno.removeSync(tempFilePath);
+	await kv.close();
+	Deno.removeSync(tempFilePath);
 });
