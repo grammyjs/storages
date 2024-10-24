2a0b4160bd6d1b20e5cba7f2afe9e9d1ca1e9c2a
Satont <satontworldwide@gmail.com>
2024-10-24 14:03:45 +0300

    rewrite psql to accept interfaces instead of actual implementations of pg clients

--- packages/psql/__tests__/node.ts
+++ packages/psql/__tests__/node.ts
@@ -1,50 +1,50 @@
 import test from 'node:test';
-import assert from 'node:assert';
+import * as assert from 'node:assert';
 
 import { newDb } from 'pg-mem';
-import { PsqlAdapter } from '../dist/cjs/mod';
+import { PsqlAdapter } from '../src/mod.ts';
 import { session } from 'grammy';
 import * as utils from '@grammyjs/storage-utils';
 
 test('Pizza counter test', async () => {
-  const bot = utils.createBot();
-  const client = new (newDb().adapters.createPg().Client);
+	const bot = utils.createBot();
+	const client = new (newDb().adapters.createPg().Client)();
 
-  bot.use(session({
-    initial: () => ({ pizzaCount: 0 }),
-    storage: await PsqlAdapter.create({ tableName: 'sessions', client }),
-  }));
+	bot.use(session({
+		initial: () => ({ pizzaCount: 0 }),
+		storage: await PsqlAdapter.create({ tableName: 'sessions', client }),
+	}));
 
-  bot.hears('first', (ctx) => {
-    assert.equal(ctx.session.pizzaCount, 0);
-    ctx.session.pizzaCount = Number(ctx.session.pizzaCount) + 1;
-  });
-  
-  bot.hears('second', (ctx) => {
-    assert.equal(ctx.session.pizzaCount, 1);
-  });
-  
-  await bot.handleUpdate(utils.createMessage(bot, 'first').update);
-  await bot.handleUpdate(utils.createMessage(bot, 'second').update);
+	bot.hears('first', (ctx) => {
+		assert.equal(ctx.session.pizzaCount, 0);
+		ctx.session.pizzaCount = Number(ctx.session.pizzaCount) + 1;
+	});
+
+	bot.hears('second', (ctx) => {
+		assert.equal(ctx.session.pizzaCount, 1);
+	});
+
+	await bot.handleUpdate(utils.createMessage(bot, 'first').update);
+	await bot.handleUpdate(utils.createMessage(bot, 'second').update);
 });
 
 test('Should be changed', async () => {
-  const client = new (newDb().adapters.createPg().Client);
-  const bot = utils.createBot(false);
+	const client = new (newDb().adapters.createPg().Client)();
+	const bot = utils.createBot(false);
 
-  bot.use(session({
-    initial: () => 'test',
-    storage: await PsqlAdapter.create({ tableName: 'sessions', client }),
-  }));
+	bot.use(session({
+		initial: () => 'test',
+		storage: await PsqlAdapter.create({ tableName: 'sessions', client }),
+	}));
 
-  bot.hears('first', async (ctx) => {
-    ctx.session = `${ctx.session} edited`;
-  });
-  
-  bot.hears('second', async (ctx) => {
-    assert.equal(ctx.session, 'test edited');
-  });
-  
-  await bot.handleUpdate(utils.createMessage(bot, 'first').update);
-  await bot.handleUpdate(utils.createMessage(bot, 'second').update);
+	bot.hears('first', async (ctx) => {
+		ctx.session = `${ctx.session} edited`;
+	});
+
+	bot.hears('second', async (ctx) => {
+		assert.equal(ctx.session, 'test edited');
+	});
+
+	await bot.handleUpdate(utils.createMessage(bot, 'first').update);
+	await bot.handleUpdate(utils.createMessage(bot, 'second').update);
 });
