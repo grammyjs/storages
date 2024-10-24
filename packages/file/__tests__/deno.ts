a6a0e8cd50e05d07ff3312edba39d0fa7feba100
Satont <satontworldwide@gmail.com>
2024-10-24 11:41:26 +0300

    fix node tests for `file` storage

--- packages/file/__tests__/deno.ts
+++ packages/file/__tests__/deno.ts
@@ -1,61 +1,62 @@
 import { session } from 'grammy';
 import { expect } from 'https://deno.land/x/expect@v0.4.0/mod.ts';
 import { FileAdapter } from '../src/mod.ts';
-import { createMessage, createBot } from '../../../libs/utils/src/mod.ts';
+import { createBot, createMessage } from '@grammyjs/storage-utils';
 import path from 'node:path';
 import { existsSync } from 'node:fs';
+import { rm as remove } from 'node:fs/promises';
 
 const dirPath = path.resolve(Deno.cwd(), 'sessions');
-const cleanDir = () => Deno.remove(dirPath, { recursive: true });
+const cleanDir = () => remove(dirPath, { recursive: true });
 
 Deno.test('Should create sessions dir', async () => {
-  new FileAdapter({ dirName: 'sessions' });
-  expect(existsSync(dirPath)).toBe(true);
+	new FileAdapter({ dirName: 'sessions' });
+	expect(existsSync(dirPath)).toBe(true);
 
-  await cleanDir();
+	await cleanDir();
 });
- 
+
 Deno.test('Pizza counter tests', async () => {
-  const bot = createBot();
+	const bot = createBot();
 
-  bot.use(session({
-    initial: () => ({ pizzaCount: 0 }),
-    storage: new FileAdapter({ dirName: 'sessions' }),
-  }));
+	bot.use(session({
+		initial: () => ({ pizzaCount: 0 }),
+		storage: new FileAdapter({ dirName: 'sessions' }),
+	}));
 
-  bot.hears('first', (ctx) => {
-    expect(ctx.session.pizzaCount).toEqual(0);
-    ctx.session.pizzaCount = Number(ctx.session.pizzaCount) + 1;
-  });
-  
-  bot.hears('second', (ctx) => {
-    expect(ctx.session.pizzaCount).toEqual(1);
-  });
-  
-  await bot.handleUpdate(createMessage(bot, 'first').update);
-  await bot.handleUpdate(createMessage(bot, 'second').update);
+	bot.hears('first', (ctx) => {
+		expect(ctx.session.pizzaCount).toEqual(0);
+		ctx.session.pizzaCount = Number(ctx.session.pizzaCount) + 1;
+	});
 
-  await cleanDir();
+	bot.hears('second', (ctx) => {
+		expect(ctx.session.pizzaCount).toEqual(1);
+	});
+
+	await bot.handleUpdate(createMessage(bot, 'first').update);
+	await bot.handleUpdate(createMessage(bot, 'second').update);
+
+	await cleanDir();
 });
 
 Deno.test('Simple string tests', async () => {
-  const bot = createBot(false);
+	const bot = createBot(false);
 
-  bot.use(session({
-    initial: () => 'test',
-    storage: new FileAdapter({ dirName: 'sessions' }),
-  }));
+	bot.use(session({
+		initial: () => 'test',
+		storage: new FileAdapter({ dirName: 'sessions' }),
+	}));
 
-  bot.hears('first', async (ctx) => {
-    ctx.session = `${ctx.session} edited`;
-  });
-  
-  bot.hears('second', async (ctx) => {
-    expect(ctx.session).toEqual('test edited');
-  });
-  
-  await bot.handleUpdate(createMessage(bot, 'first').update);
-  await bot.handleUpdate(createMessage(bot, 'second').update);
+	bot.hears('first', async (ctx) => {
+		ctx.session = `${ctx.session} edited`;
+	});
 
-  await cleanDir();
+	bot.hears('second', async (ctx) => {
+		expect(ctx.session).toEqual('test edited');
+	});
+
+	await bot.handleUpdate(createMessage(bot, 'first').update);
+	await bot.handleUpdate(createMessage(bot, 'second').update);
+
+	await cleanDir();
 });
--- packages/file/__tests__/node.ts
+++ packages/file/__tests__/node.ts
@@ -11,57 +11,58 @@ import { existsSync } from 'node:fs';
 
 const dirPath = resolve(cwd(), 'sessions');
 const cleanDir = async () => {
-  await remove(dirPath).catch(() => null);
+	await remove(dirPath, { recursive: true });
 };
 
 test('Should create sessions dir', async () => {
-  await cleanDir();
+	await cleanDir();
 
-  new FileAdapter({ dirName: 'sessions' });
-  assert.ok(existsSync(dirPath));
+	new FileAdapter({ dirName: 'sessions' });
+	assert.ok(existsSync(dirPath));
 });
 
 test('Pizza counter tests', async () => {
-  await cleanDir();
+	await cleanDir();
 
-  const bot = createBot();
+	const bot = createBot();
 
-  bot.use(session({
-    initial: () => ({ pizzaCount: 0 }),
-    storage: new FileAdapter({ dirName: 'sessions' }),
-  }));
+	bot.use(session({
+		initial: () => ({ pizzaCount: 0 }),
+		storage: new FileAdapter({ dirName: 'sessions' }),
+	}));
 
-  bot.hears('first', (ctx) => {
-    assert.equal(ctx.session.pizzaCount, 0);
-    ctx.session.pizzaCount = Number(ctx.session.pizzaCount) + 1;
-  });
+	bot.hears('first', (ctx) => {
+		console.log('session', ctx.session);
+		assert.equal(ctx.session.pizzaCount, 0);
+		ctx.session.pizzaCount = ctx.session.pizzaCount + 1;
+	});
 
-  bot.hears('second', (ctx) => {
-    assert.equal(ctx.session.pizzaCount, 1);
-  });
+	bot.hears('second', (ctx) => {
+		assert.equal(ctx.session.pizzaCount, 1);
+	});
 
-  await bot.handleUpdate(createMessage(bot, 'first').update);
-  await bot.handleUpdate(createMessage(bot, 'second').update);
+	await bot.handleUpdate(createMessage(bot, 'first').update);
+	await bot.handleUpdate(createMessage(bot, 'second').update);
 });
 
 test('Simple string tests', async () => {
-  await cleanDir();
+	await cleanDir();
 
-  const bot = createBot(false);
+	const bot = createBot(false);
 
-  bot.use(session({
-    initial: () => 'test',
-    storage: new FileAdapter({ dirName: 'sessions' }),
-  }));
+	bot.use(session({
+		initial: () => 'test',
+		storage: new FileAdapter({ dirName: 'sessions' }),
+	}));
 
-  bot.hears('first', async (ctx) => {
-    ctx.session = `${ctx.session} edited`;
-  });
+	bot.hears('first', (ctx) => {
+		ctx.session = `${ctx.session} edited`;
+	});
 
-  bot.hears('second', async (ctx) => {
-    assert.equal(ctx.session, 'test edited');
-  });
+	bot.hears('second', (ctx) => {
+		assert.equal(ctx.session, 'test edited');
+	});
 
-  await bot.handleUpdate(createMessage(bot, 'first').update);
-  await bot.handleUpdate(createMessage(bot, 'second').update);
+	await bot.handleUpdate(createMessage(bot, 'first').update);
+	await bot.handleUpdate(createMessage(bot, 'second').update);
 });
