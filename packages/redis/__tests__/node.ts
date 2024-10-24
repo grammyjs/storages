1081b792ba7def0e588de45dde27ecfb5e11b51a
Satont <satontworldwide@gmail.com>
2024-10-24 14:26:57 +0300

    update redis package, accept interface of client instead of actual packages

--- packages/redis/__tests__/node.ts
+++ packages/redis/__tests__/node.ts
@@ -2,64 +2,53 @@ import test from 'node:test';
 import assert from 'node:assert';
 
 import { Bot, Context, session } from 'grammy';
-import { RedisAdapter } from '../dist/esm/mod';
-import { RedisMock } from './redisMock';
+import { RedisAdapter } from '../src/mod.ts';
+import { RedisMock } from './redisMock.ts';
 import { createBot, createMessage } from '@grammyjs/storage-utils';
 
 test('Pizza counter tests', async () => {
-  const bot = createBot();
+	const bot = createBot(true);
 
-  bot.use(session({
-    initial: () => ({ pizzaCount: 0 }),
-    storage: new RedisAdapter({ instance: new RedisMock() as any }),
-  }));
+	bot.use(session({
+		initial: () => ({ pizzaCount: 0 }),
+		storage: new RedisAdapter({ instance: new RedisMock() }),
+	}));
 
-  bot.hears('first', (ctx) => {
-    assert.equal(ctx.session.pizzaCount, 0);
-    ctx.session.pizzaCount = Number(ctx.session.pizzaCount) + 1;
-  });
-  
-  bot.hears('second', (ctx) => {
-    assert.equal(ctx.session.pizzaCount, 1);
-  });
-  
-  await bot.handleUpdate(createMessage(bot, 'first').update);
-  await bot.handleUpdate(createMessage(bot, 'second').update);
+	bot.hears('first', (ctx) => {
+		assert.equal(ctx.session.pizzaCount, 0);
+		ctx.session.pizzaCount = Number(ctx.session.pizzaCount) + 1;
+	});
+
+	bot.hears('second', (ctx) => {
+		assert.equal(ctx.session.pizzaCount, 1);
+	});
+
+	await bot.handleUpdate(createMessage(bot, 'first').update);
+	await bot.handleUpdate(createMessage(bot, 'second').update);
 });
- 
 
 test('Simple string tests', async () => {
-  interface StringSessionFlavor {
-    get session(): string;
-    set session(session: string | null | undefined);
-  }
+	interface StringSessionFlavor {
+		get session(): string;
+		set session(session: string | null | undefined);
+	}
 
-  const bot = new Bot<Context & StringSessionFlavor>('fake-token', { 
-    botInfo: {
-      id: 42,
-      first_name: 'Test Bot',
-      is_bot: true,
-      username: 'bot',
-      can_join_groups: true,
-      can_read_all_group_messages: true,
-      supports_inline_queries: false,
-    },
-  });
+	const bot = createBot(false);
 
-  bot.use(session({
-    initial: () => 'test',
-    storage: new RedisAdapter({ instance: new RedisMock() as any }),
-  }));
+	bot.use(session({
+		initial: () => 'test',
+		storage: new RedisAdapter({ instance: new RedisMock() as any }),
+	}));
 
-  bot.hears('first', async (ctx) => {
-    assert.equal(ctx.session, 'test');
-    ctx.session = `${ctx.session} edited`;
-  });
-  
-  bot.hears('second', async (ctx) => {
-    assert.equal(ctx.session, 'test edited');
-  });
-  
-  await bot.handleUpdate(createMessage(bot, 'first').update);
-  await bot.handleUpdate(createMessage(bot, 'second').update);
+	bot.hears('first', async (ctx) => {
+		assert.equal(ctx.session, 'test');
+		ctx.session = `${ctx.session} edited`;
+	});
+
+	bot.hears('second', async (ctx) => {
+		assert.equal(ctx.session, 'test edited');
+	});
+
+	await bot.handleUpdate(createMessage(bot, 'first').update);
+	await bot.handleUpdate(createMessage(bot, 'second').update);
 });
--- packages/redis/src/mod.ts
+++ packages/redis/src/mod.ts
@@ -1,60 +1,72 @@
-import { Client, StorageAdapter } from './deps.deno.ts';
+import type { StorageAdapter } from 'grammy';
+
+interface Client {
+	get: (key: string) => Promise<string | null>;
+	set: (key: string, value: string) => Promise<void>;
+	expire: (key: string, ttl: number) => Promise<void>;
+	del: (key: string) => Promise<void>;
+}
 
 export class RedisAdapter<T> implements StorageAdapter<T> {
-  private redis: Client;
-  private readonly ttl?: number;
-  private readonly autoParseDates: boolean;
+	private redis: Client;
+	private readonly ttl?: number;
+	private readonly autoParseDates: boolean;
 
-  /**
-   * @constructor
-   * @param {opts} Constructor options
-   * @param {opts.ttl} ttl - Session time to life in SECONDS.
-   * @param {opts.instance} instance - Instance of redis.
-   * @param {opts.autoParseDates} autoParseDates - set to true to convert string in the json date format to date object
-   */
-  constructor({ instance, ttl, autoParseDates }: { instance?: Client; ttl?: number, autoParseDates?: boolean }) {
-    if (instance) {
-      this.redis = instance;
-    } else {
-      throw new Error('You should pass redis instance to constructor.');
-    }
+	/**
+	 * @constructor
+	 * @param {opts} Constructor options
+	 * @param {opts.ttl} ttl - Session time to life in SECONDS.
+	 * @param {opts.instance} instance - Instance of redis.
+	 * @param {opts.autoParseDates} autoParseDates - set to true to convert string in the json date format to date object
+	 */
+	constructor(
+		{ instance, ttl, autoParseDates }: {
+			instance?: Client;
+			ttl?: number;
+			autoParseDates?: boolean;
+		},
+	) {
+		if (instance) {
+			this.redis = instance;
+		} else {
+			throw new Error('You should pass redis instance to constructor.');
+		}
 
-    this.ttl = ttl;
-    this.autoParseDates = autoParseDates || false;
-  }
+		this.ttl = ttl;
+		this.autoParseDates = autoParseDates || false;
+	}
 
-  async read(key: string) {
-    const session = await this.redis.get(key);
-    if (session === null || session === undefined) {
-      return undefined;
-    }
-    if (this.autoParseDates) {
-      return JSON.parse(session, dateParser) as unknown as T;
-    }
-    return JSON.parse(session) as unknown as T;
-  }
+	async read(key: string) {
+		const session = await this.redis.get(key);
+		if (session === null || session === undefined) {
+			return undefined;
+		}
+		if (this.autoParseDates) {
+			return JSON.parse(session, dateParser) as unknown as T;
+		}
+		return JSON.parse(session) as unknown as T;
+	}
 
-  async write(key: string, value: T) {
-    await this.redis.set(key, JSON.stringify(value));
-    if (this.ttl) {
-      this.redis.expire(key, this.ttl);
-    }
-  }
+	async write(key: string, value: T) {
+		await this.redis.set(key, JSON.stringify(value));
+		if (this.ttl) {
+			this.redis.expire(key, this.ttl);
+		}
+	}
 
-  async delete(key: string) {
-    await this.redis.del(key);
-  }
+	async delete(key: string) {
+		await this.redis.del(key);
+	}
 }
 
 const ISO_8601 = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d+)?Z?$/;
-const dateParser = (key: string, value: any) => {
-
-  if (typeof value === 'string' && ISO_8601.test(value)) {
-    let newValue;
-    if (!value.endsWith('Z')) {
-      newValue = `${value}Z`;
-    } else newValue = value;
-    return new Date(newValue);
-  }
-  return value;
+const dateParser = (_key: string, value: any) => {
+	if (typeof value === 'string' && ISO_8601.test(value)) {
+		let newValue;
+		if (!value.endsWith('Z')) {
+			newValue = `${value}Z`;
+		} else newValue = value;
+		return new Date(newValue);
+	}
+	return value;
 };
