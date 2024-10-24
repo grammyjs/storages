5eaaed4ada58444ddab64db549149fdf38ef3ef2
Satont <satontworldwide@gmail.com>
2024-10-24 15:43:00 +0300

    update psql tests

--- packages/psql/__tests__/node.ts
+++ packages/psql/__tests__/node.ts
@@ -6,40 +6,7 @@ import pg from 'pg';
 import { session } from 'grammy';
 import * as utils from '@grammyjs/storage-utils';
 
-const client = new pg.Client({
-	user: 'postgres',
-	password: 'postgres',
-	database: 'postgres',
-	host: 'localhost',
-	port: 5432,
-});
-
-test.after(async () => {
-	await client.end();
-});
-
-test('Pizza counter test', async () => {
-	const bot = utils.createBot();
-
-	bot.use(session({
-		initial: () => ({ pizzaCount: 0 }),
-		storage: await PsqlAdapter.create({ tableName: 'sessions', client }),
-	}));
-
-	bot.hears('first', (ctx) => {
-		assert.equal(ctx.session.pizzaCount, 0);
-		ctx.session.pizzaCount = Number(ctx.session.pizzaCount) + 1;
-	});
-
-	bot.hears('second', (ctx) => {
-		assert.equal(ctx.session.pizzaCount, 1);
-	});
-
-	await bot.handleUpdate(utils.createMessage(bot, 'first').update);
-	await bot.handleUpdate(utils.createMessage(bot, 'second').update);
-});
-
-test('Should be changed', async () => {
+test.describe('Tests', () => {
 	const client = new pg.Client({
 		user: 'postgres',
 		password: 'postgres',
@@ -47,21 +14,56 @@ test('Should be changed', async () => {
 		host: 'localhost',
 		port: 5432,
 	});
-	const bot = utils.createBot(false);
 
-	bot.use(session({
-		initial: () => 'test',
-		storage: await PsqlAdapter.create({ tableName: 'sessions', client }),
-	}));
-
-	bot.hears('first', async (ctx) => {
-		ctx.session = `${ctx.session} edited`;
+	test.after(async () => {
+		await client.end();
 	});
 
-	bot.hears('second', async (ctx) => {
-		assert.equal(ctx.session, 'test edited');
+	test.it('Pizza counter test', async () => {
+		const bot = utils.createBot();
+
+		bot.use(session({
+			initial: () => ({ pizzaCount: 0 }),
+			storage: await PsqlAdapter.create({ tableName: 'sessions', client }),
+		}));
+
+		bot.hears('first', (ctx) => {
+			assert.equal(ctx.session.pizzaCount, 0);
+			ctx.session.pizzaCount = Number(ctx.session.pizzaCount) + 1;
+		});
+
+		bot.hears('second', (ctx) => {
+			assert.equal(ctx.session.pizzaCount, 1);
+		});
+
+		await bot.handleUpdate(utils.createMessage(bot, 'first').update);
+		await bot.handleUpdate(utils.createMessage(bot, 'second').update);
 	});
 
-	await bot.handleUpdate(utils.createMessage(bot, 'first').update);
-	await bot.handleUpdate(utils.createMessage(bot, 'second').update);
+	test('Should be changed', async () => {
+		const client = new pg.Client({
+			user: 'postgres',
+			password: 'postgres',
+			database: 'postgres',
+			host: 'localhost',
+			port: 5432,
+		});
+		const bot = utils.createBot(false);
+
+		bot.use(session({
+			initial: () => 'test',
+			storage: await PsqlAdapter.create({ tableName: 'sessions', client }),
+		}));
+
+		bot.hears('first', async (ctx) => {
+			ctx.session = `${ctx.session} edited`;
+		});
+
+		bot.hears('second', async (ctx) => {
+			assert.equal(ctx.session, 'test edited');
+		});
+
+		await bot.handleUpdate(utils.createMessage(bot, 'first').update);
+		await bot.handleUpdate(utils.createMessage(bot, 'second').update);
+	});
 });
